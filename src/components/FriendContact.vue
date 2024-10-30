9322f6bd64f929e3ad9a6117d352acb4be15a26c
John Doe <johndoe@example.com>
2024-10-30 10:35:28 +0100

    Props is-favorite -> isFavorite

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Friend Contact</h1>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">ToggleFavorite</button>_
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+      <li><strong>Email: </strong>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
+      // friend: {
+      //   id: "manuel",
+      //   name: "Manuel Gonzales",
+      //   phone: "0986533",
+      //   email: "manule@gmail.com",
+      // },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
+      }
+    },
+  },
+};
+</script>
