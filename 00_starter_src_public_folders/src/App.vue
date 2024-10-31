d05e685d87c2533dc7e3ab03c3359661bf0ac2ec
John Doe <johndoe@example.com>
2024-10-31 11:13:26 +0100

    Section9. Start Slots

---
+++ 00_starter_src_public_folders/src/App.vue
@@ -0,0 +1,164 @@
+<template>
+  <section>
+    <header><h1>My friends</h1></header>
+    <!-- <new-form @named-emit-event="namedEmitEvent"></new-form> -->
+    <new-friend @add-contact="addContact"></new-friend>
+    <ul>
+      <!-- toggle-favorite is emitted from FriendContact -->
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :user-name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+// import FriendContact from "./components/FriendContact.vue";
+export default {
+  // components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Gonzales",
+          phone: "0986533",
+          email: "manule@gmail.com",
+          isFavorite: true,
+        },
+        {
+          id: "julie",
+          name: "Julie Gonzales",
+          phone: "4560986533",
+          email: "julie@gmail.com",
+          isFavorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+      console.log(friendId);
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      console.log(newFriendContact);
+      this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId) {
+      this.friends = this.friends.filter((friend) => friend.id !== friendId);
+    },
+    // namedEmitEvent(name, phone, email) {
+    //   const newFriendContact = {
+    //     id: new Date().toISOString(),
+    //     name: name,
+    //     phone: phone,
+    //     email: email,
+    //     isFavorite: false,
+    //   };
+    //   console.log(newFriendContact);
+    //   this.friends.push(newFriendContact);
+    // },
+  },
+  computed: {},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app form,
+li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+</style>
